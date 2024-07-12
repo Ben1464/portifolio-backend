44797a19d50f6a187fa647a7a3e75f2441cd44dd
Ben <kimaniben14@gmail.com>
2024-07-12 13:58:47 +0100

    updates

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors(
   {
-  origin:"https://portifolio-eight-ashen-62.vercel.app/"
+  origin:"https://benson14.vercel.app/"
   }
 ));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://portifolio-eight-ashen-62.vercel.app/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://benson14.vercel.app/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
